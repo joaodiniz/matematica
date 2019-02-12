8ba7220ad034eba37af69c0859a6f2ffed5442dd
joaodiniz <12045@aebenfica.org>
2019-02-12 13:03:26 +0000

    yey

--- codigo.js
+++ codigo.js
@@ -1,5 +1,5 @@
     var num1, num2, sum
-    num1 = prompt("5")
-    num2 = prompt("2")
+    num1 = prompt("Mete número")
+    num2 = prompt("Mete número")
     sum = parseInt(num1) + parseInt(num2) // "+" means "add"
     alert("Soma = " + sum)  // "+" means combine into a string
